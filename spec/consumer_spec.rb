3b202599839c995b30a16c5186d9d741b78b23e4
Bibek Shrestha <bibekshrestha@gmail.com>
2018-09-26 10:10:01 -0400

    Move exit_if early in consumer_loop

--- spec/consumer_spec.rb
+++ spec/consumer_spec.rb
@@ -224,6 +224,35 @@ describe Kafka::Consumer do
       end
     end
 
+    context "when fetch_batches returns FetchError" do
+      let(:thread_timeout) { 3 }
+      before do
+        allow(cluster).to receive(:mark_as_stale!)
+        allow(consumer).to receive(:fetch_batches).and_raise(Kafka::FetchError)
+      end
+
+      it "runs forever if exit_if is not passed" do
+        th = Thread.new do
+          consumer.each_batch do |message|
+            fail "this block is never called"
+          end
+        end
+
+        expect(th.join(thread_timeout)).to be_nil # join returning nil is a sign of thread not completing within timeout
+        th.kill
+      end
+
+      it "aborts earlier if exit_if is passed" do
+        th = Thread.new do
+          consumer.each_batch(exit_if: -> { true } ) do |message|
+            fail "this block is never called"
+          end
+        end
+
+        expect(th.join(thread_timeout)).to eq th # join returning itself means that thread is done
+      end
+    end
+
     it "does not mark as processed when automatically_mark_as_processed is false" do
       expect(offset_manager).not_to receive(:mark_as_processed)
 
